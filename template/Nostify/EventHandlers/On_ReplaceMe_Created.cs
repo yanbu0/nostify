45797b8f257eb25ebaad743d2c6ef3cf9730a595
yanbu0 <boyle_travis@hotmail.com>
2023-10-06 08:36:17 -0800

    added create handler stub

---
+++ template/Nostify/EventHandlers/On_ReplaceMe_Created.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using nostify;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace _ReplaceMe__Service
+{
+    public class On_ReplaceMe_Created
+    {
+
+        private readonly HttpClient _client;
+        private readonly Nostify _nostify;
+        public On_ReplaceMe_Created(HttpClient httpClient, Nostify nostify)
+        {
+            this._client = httpClient;
+            this._nostify = nostify;;
+        }
+
+        [FunctionName("On_ReplaceMe_Created")]
+        public async Task Run([CosmosDBTrigger(
+            databaseName: "_ReplaceMe__DB",
+            collectionName: "persistedEvents",
+            ConnectionStringSetting = "<YourConnectionStringHere>",
+            CreateLeaseCollectionIfNotExists = true,
+            LeaseCollectionPrefix = "On_ReplaceMe_Created_",
+            LeaseCollectionName = "leases")] IReadOnlyList<Document> input,
+            ILogger log)
+        {
+            if (input != null)
+            {
+                foreach (Document doc in input)
+                {
+                    PersistedEvent pe = null;
+                    try
+                    {
+                        pe = JsonConvert.DeserializeObject<PersistedEvent>(doc.ToString());
+
+                        var agg = new _ReplaceMe_();
+                        agg.Apply(pe);
+
+                        //Update aggregate current state projection
+                        Container currentStateContainer = await _nostify.GetCurrentStateContainerAsync();
+                        await currentStateContainer.UpsertItemAsync<_ReplaceMe_>(agg);                            
+
+                    }
+                    catch (Exception e)
+                    {
+                        await _nostify.HandleUndeliverableAsync("On_ReplaceMe_Deleted", e.Message, pe);
+                    }
+
+                }
+            }
+        }
+    }
+}
