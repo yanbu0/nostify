9994bea6080b928b3dff2ef0bb2d022dd561a341
yanbu0 <boyle_travis@hotmail.com>
2023-11-01 08:51:26 -0800

    implemented PublishEventAsync

--- Nostify.cs
+++ Nostify.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos.Linq;
+using Newtonsoft.Json;
+using Confluent.Kafka;
 
 namespace nostify
 {
@@ -15,11 +17,12 @@ namespace nostify
         NostifyCosmosClient Repository { get; }
         string DefaultPartitionKeyPath { get; }
         int DefaultTenantId { get; }
+        string KafkaUrl { get; }
 
         public Task PersistAsync(PersistedEvent persistedEvent);
         public Task BulkPersistAsync(List<PersistedEvent> persistedEvents);
         public Task HandleUndeliverableAsync(string functionName, string errorMessage, PersistedEvent persistedEvent);
-        public Task PublishEventAsync(PersistedEvent persistedEvent);
+        public Task PublishEventAsync(string cosmosTriggerOutput);
         public Task<Container> GetPersistedEventsContainerAsync(bool allowBulk = false);
         public Task<Container> GetCurrentStateContainerAsync(string partitionKeyPath = "/tenantId");
 
@@ -45,6 +48,11 @@ namespace nostify
         ///Default tenannt id value for use if NOT implementing multi-tenant
         ///</summary>
         public int DefaultTenantId { get; }
+
+        ///<summary>
+        ///Url for Kafka cluster
+        ///</summary>
+        public string KafkaUrl { get; }
         
 
         ///<summary>
@@ -52,17 +60,19 @@ namespace nostify
         ///</summary>
         ///<param name="Primarykey">API Key value</param>
         ///<param name="DbName">Name of event store DB</param>
-        ///<param name="EndpointUri">Uri of cosmos endpoint, format: https://[instance-name].documents.azure.us:443/</param>
+        ///<param name="CosmosEndpointUri">Uri of cosmos endpoint, format: https://[instance-name].documents.azure.us:443/</param>
+        ///<param name="KafkaUrl">Url of Kafka instance, format: localhost:54165</param>
         ///<param name="DefaultPartitionKeyPath">Path to partition key for default created current state container, set null to not create, leave default to partition by tenantId </param>
         ///<param name="DefaultTenantId">Default tenannt id value for use if NOT implementing multi-tenant, if left to default will create one partition in current state container</param>
-        public Nostify(string Primarykey, string DbName, string EndpointUri, string DefaultPartitionKeyPath = "/tenantId", int DefaultTenantId = 0)
+        public Nostify(string Primarykey, string DbName, string CosmosEndpointUri, string KafkaUrl, string DefaultPartitionKeyPath = "/tenantId", int DefaultTenantId = 0)
         {
-            Repository = new NostifyCosmosClient(Primarykey, DbName, EndpointUri);
+            Repository = new NostifyCosmosClient(Primarykey, DbName, CosmosEndpointUri);
             if (DefaultPartitionKeyPath != null)
             {
                 this.DefaultPartitionKeyPath = DefaultPartitionKeyPath;
             }
             this.DefaultTenantId = DefaultTenantId;
+            this.KafkaUrl = KafkaUrl;
         }
 
 
@@ -87,16 +97,27 @@ namespace nostify
         ///<summary>
         ///Published event to messaging bus
         ///</summary>        
-        ///<param name="persistedEvent">Event to apply and persist in event store</param>
-        public async Task PublishEventAsync(PersistedEvent persistedEvent)
+        ///<param name="cosmosTriggerOutput">String output from CosmosDBTrigger, will be processed into a List of PersistedEvents and published to Kafka</param>
+        public async Task PublishEventAsync(string cosmosTriggerOutput)
         {
-            try
+            var peList = JsonConvert.DeserializeObject<List<PersistedEvent>>(cosmosTriggerOutput);
+            if (peList != null)
             {
-                throw new NotImplementedException();
-            }
-            catch (Exception e)
-            {
-                await HandleUndeliverableAsync(nameof(PublishEventAsync), e.Message, persistedEvent);
+                foreach (PersistedEvent pe in peList)
+                {
+                    var config = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("bootstrap.servers", KafkaUrl)
+                    };
+
+
+                    using (var p = new ProducerBuilder<string,string>(config).Build())
+                    {
+                        string topic = "Location" + pe.command.name;
+                        var result = await p.ProduceAsync(topic, new Message<string, string>{  Value = JsonConvert.SerializeObject(pe) });
+                    }         
+
+                }
             }
         }
 
