a02d84bde69dc5e635c574e724b91db208c064cd
rsam-asrc <robin.sam@asrcfederal.com>
2024-07-25 08:55:01 -0700

    update solution file with aggregates

--- end-user-scripts/vscodeAddServices.ps1
+++ end-user-scripts/vscodeAddServices.ps1
@@ -106,6 +106,105 @@ function New-WorkspaceFile {
     Write-Output "New workspace file created."
 }
 
+function New-SolutionFile {
+    param (
+        [string]$solutionFile
+    )
+    $solutionGuid = [guid]::NewGuid().ToString()
+
+    $solutionContent = @"
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.0.0.0
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+    GlobalSection(SolutionConfigurationPlatforms) = preSolution
+        Debug|Any CPU = Debug|Any CPU
+        Release|Any CPU = Release|Any CPU
+    EndGlobalSection
+    GlobalSection(ProjectConfigurationPlatforms) = postSolution
+    EndGlobalSection
+    GlobalSection(SolutionProperties) = preSolution
+        HideSolutionNode = FALSE
+    EndGlobalSection
+    GlobalSection(ExtensibilityGlobals) = postSolution
+        SolutionGuid = {$solutionGuid}
+    EndGlobalSection
+EndGlobal
+"@
+
+    Set-Content $solutionFile -Value $solutionContent
+
+    Write-Output "Solution file created successfully: $solutionFile"
+}
+
+function Add-ProjectToSolution {
+    param (
+        [string]$solutionFile,
+        [string]$projectFile,
+        [string]$projectName
+    )
+    # check solution file exists, create if it doesn't
+    if (-Not (Test-Path $solutionFile)) {
+        Write-Output "Creating new solution file: $solutionFile"
+        New-SolutionFile -SolutionFile $solutionFile
+    }
+
+    # read the solution file
+    $solutionContent = Get-Content $solutionFile
+
+    # check if the project is in the solution
+    if ($solutionContent -join "`n" -match [regex]::Escape($projectFile)) {
+        return
+    }
+
+    $projectGuid = [guid]::NewGuid().ToString()
+    $projectEntry = @"
+Project("{$projectGuid}") = "$projectName", "$projectFile", "{$projectGuid}"
+EndProject
+"@
+
+    # find the Global section
+    $globalSectionIndex = $solutionContent.IndexOf("Global")
+    if ($globalSectionIndex -lt 0) {
+        Write-Error "Global section not found in the solution file"
+        return
+    }
+
+    # insert the project entry before the Global section
+    $solutionContent = $solutionContent[0..($globalSectionIndex - 1)] + $projectEntry + $solutionContent[$globalSectionIndex..($solutionContent.Length - 1)]
+
+    # find the ProjectConfigurationPlatforms section - need to use a regex over each line to match without leading indentation
+    $projectConfigSectionIndex = -1
+    for ($i = 0; $i -lt $solutionContent.Length; $i++) {
+        if ($solutionContent[$i] -match "^\s*GlobalSection\(ProjectConfigurationPlatforms\) = postSolution") {
+            $projectConfigSectionIndex = $i
+            break
+        }
+    }
+    if ($projectConfigSectionIndex -lt 0) {
+        Write-Error "ProjectConfigurationPlatforms section not found in the solution file"
+        return
+    }
+
+    # project configurations
+    $projectConfigurations = @"
+        {$projectGuid}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+        {$projectGuid}.Debug|Any CPU.Build.0 = Debug|Any CPU
+        {$projectGuid}.Release|Any CPU.ActiveCfg = Release|Any CPU
+        {$projectGuid}.Release|Any CPU.Build.0 = Release|Any CPU
+"@
+
+    # insert the project configurations before the EndGlobalSection of the ProjectConfigurationPlatforms section
+    $endProjectConfigSectionIndex = $solutionContent[$projectConfigSectionIndex..($solutionContent.Length - 1)].IndexOf("EndGlobalSection") + $projectConfigSectionIndex
+    $solutionContent = $solutionContent[0..($endProjectConfigSectionIndex - 1)] + $projectConfigurations + $solutionContent[$endProjectConfigSectionIndex..($solutionContent.Length - 1)]
+
+    # write the updated solution file
+    Set-Content $SolutionFile -Value $solutionContent
+
+    Write-Output "Added '$projectName' to solution"
+}
+
 $rootFolder = Get-Location
 
 # get all service folders containing a %ServiceName%_Service.csproj file
@@ -129,7 +228,24 @@ else {
     $workspaceFilePath = $workspaceFile.FullName
 }
 
+# get the .sln file in the root folder
+$solutionFile = Get-ChildItem -Path $rootFolder -Filter "*.sln" -Force | Select-Object -First 1
+
+if (-not $solutionFile) {
+    # create a default solution file
+    $solutionFilePath = Join-Path $rootFolder "Microservices.sln"
+}
+else {
+    $solutionFilePath = $solutionFile.FullName
+}
+
 # update the workspace file with found services
 foreach ($serviceProject in $serviceProjects) {
     Update-WorkspaceFile -workspaceFilePath $workspaceFilePath -serviceName $serviceProject.Name
+}
+
+# update the solution file with found services
+foreach ($serviceProject in $serviceProjects) {
+    $projectFile = Join-Path $serviceProject.Path "$($serviceProject.Name)_Service.csproj"
+    Add-ProjectToSolution -solutionFile $solutionFilePath -projectFile $projectFile -projectName $serviceProject.Name
 }
