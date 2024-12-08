8f7cf948368983db1557f743eac4dbd398d37553
yanbu0 <boyle_travis@hotmail.com>
2024-12-08 08:17:15 -0900

    added using

--- nostify.Tests/ProjectionBaseClassTests.cs
+++ nostify.Tests/ProjectionBaseClassTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 using Moq;
