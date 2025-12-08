e746c6f18664718424648b9755675216f9d13e15
yanbu0 <boyle_travis@hotmail.com>
2025-12-08 08:58:05 -0800

    logging update

--- src/CosmosExtensions/ContainerExtensions.cs
+++ src/CosmosExtensions/ContainerExtensions.cs
@@ -221,7 +221,7 @@ public static class ContainerExtensions
             }
             catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
-                Console.Error.WriteLine($"Aggregate not found {idToMatch}");
+                Console.Error.WriteLine($"Aggregate not found: {container.Id} {idToMatch}, tenantId: {partitionKey}");
                 nosObjToUpdate = null;
             }
         }
